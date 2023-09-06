f2effa9be148df37de2bb7e4e9590a8b1e060b99
afzal mahmud <afzzalmahmud@gmail.com>
2023-09-06 21:59:46 +0600

    [Navbar] Re-write the whole navbar for css over-writing issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,124 +1,77 @@
-import Image from "next/image";
 import Link from "next/link";
-import React, { useState, useEffect } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import LogoImage from "@/assets/logo.png"
+import { useState } from "react";
+import LogoImage from "@/assets/logo.png";
+import Image from "next/image";
 
-const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const [shadow, setShadow] = useState(false);
-
-  const handleNav = () => {
-    setNav(!nav);
-  };
-
-  useEffect(() => {
-    const handleShadow = () => {
-      if (window.scrollY >= 90) {
-        setShadow(true);
-      } else {
-        setShadow(false);
-      }
-    };
-    window.addEventListener("scroll", handleShadow);
-  }, []);
-
+function NavBar() {
+  const [navbar, setNavbar] = useState(false);
   return (
-    <div
-      style={{ backgroundColor: "#ecf0f3" }}
-      className={
-        shadow
-          ? "fixed w-full h-20 shadow-xl z-[100] ease-in-out duration-300"
-          : "fixed w-full h-20 z-[100]"
-      }
-    >
-      <div className="flex justify-between items-center w-full h-full mt-2 px-2 2xl:px-16">
-        <Link href="/">
-            <Image src={LogoImage} width={60} height={50}/>
-        </Link>
-        <div className="flex justify-between items-center">
-          <ul className="hidden md:flex mr-3">
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/">Home</Link>
-            </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/category">Category</Link>
-            </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/login">LogIn</Link>
-            </li>
-            <li className="ml-10 text-sm uppercase hover:border-b">
-              <Link href="/login">Register</Link>
-            </li>
-          </ul>
-          <button className="mr-3 inline-block px-6 pb-2 pt-2.5 shadow-[0_4px_9px_-4px_#3b71ca]">
-                Build Your Pc
-              </button>
-          {/* Hamburger Icon */}
-          <div
-            style={{ color: "#1f2937" }}
-            onClick={handleNav}
-            className="md:hidden"
-          >
-            <AiOutlineMenu size={25} />
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile Menu */}
-      {/* Overlay */}
-      <div
-        className={
-          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
-        }
-      >
-        {/* Side Drawer Menu */}
-        <div
-          className={
-            nav
-              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] py-7 px-10 ease-in duration-500"
-              : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
-          }
-        >
+    <div>
+      <nav className="w-full bg-black fixed top-0 left-0 right-0 z-10">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
           <div>
-            <div className="flex w-full items-center justify-between">
-              <Link href="/">logo</Link>
-              <div
-                onClick={handleNav}
-                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer"
-              >
-                <AiOutlineClose />
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              <div className="flex justify-center items-center">
+                {/* LOGO */}
+                <Image src={LogoImage} width={50} height={40} />
+                <Link href="/">
+                  <h2 className="text-2xl text-white font-bold ">Pc Builder</h2>
+                </Link>
+              </div>
+              {/* HAMBURGER BUTTON FOR MOBILE */}
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <AiOutlineClose className="text-white" size={25} />
+                  ) : (
+                    <AiOutlineMenu className="text-white" size={25} />
+                  )}
+                </button>
               </div>
             </div>
           </div>
-          <div className="py-4 flex flex-col">
-            <ul className="uppercase">
-              <Link href="/">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
-                  Home
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? "p-12 md:p-0 block" : "hidden"
+              }`}
+            >
+              <ul className="h-screen md:h-auto items-center justify-center md:flex ">
+                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="/" onClick={() => setNavbar(!navbar)}>
+                    Home
+                  </Link>
                 </li>
-              </Link>
-              <Link href="/category">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
-                Category
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="/category" onClick={() => setNavbar(!navbar)}>
+                    Category
+                  </Link>
                 </li>
-              </Link>
-              <Link href="/login">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
-                LogIn
+
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                  <Link href="/login" onClick={() => setNavbar(!navbar)}>
+                    Login/Register
+                  </Link>
                 </li>
-              </Link>
-              <Link href="/login">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
-                  Register
+
+                
+
+                <li className="pb-6 text-xl text-white py-2 text-center  border-b-2 md:border-b-0">
+                  <button className="inline-block px-6 pb-2 pt-2.5 shadow-[0_4px_9px_-4px_#3b71ca]">
+                    Build Pc
+                  </button>
                 </li>
-              </Link>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+      </nav>
     </div>
   );
-};
+}
 
-export default Navbar;
+export default NavBar;
