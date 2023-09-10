34ac47ee3913cdca83646ac47be5ba7f25bd0778
afzal mahmud <afzzalmahmud@gmail.com>
2023-09-10 23:13:57 +0600

    [SSG] implementation on details page with switch-case statement

---
+++ src/pages/details/[productDetails].js
@@ -0,0 +1,68 @@
+import CpuCard from "@/components/cards/cpuCard";
+import MonitorCard from "@/components/cards/monitorCard";
+import MotherBoardsCard from "@/components/cards/motherBoardCard";
+import PowerSupplyCard from "@/components/cards/powerSupplyCard";
+import RamCard from "@/components/cards/ramCard";
+import StorageDeviceCard from "@/components/cards/storageDeviceCard";
+import NoFooterLayout from "@/layout/NoFooterLayout";
+import React, { useState } from "react";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+
+function ProductDetails({ singleProduct }) {
+  /* review for Cpu */ 
+  let ProductCardComponent;
+
+  switch (singleProduct.ProductType) {
+    case 'Monitor':
+      ProductCardComponent = <MonitorCard singleProduct={singleProduct} />;
+      break;
+    case 'StorageDevice':
+      ProductCardComponent = <StorageDeviceCard singleProduct={singleProduct} />;
+      break;
+    case 'PowerSupply':
+      ProductCardComponent = <PowerSupplyCard singleProduct={singleProduct} />;
+      break;
+    case 'Ram':
+      ProductCardComponent = <RamCard singleProduct={singleProduct} />;
+      break;
+    case 'MotherBoards':
+      ProductCardComponent = <MotherBoardsCard singleProduct={singleProduct} />;
+      break;
+    default:
+      ProductCardComponent = <CpuCard singleProduct={singleProduct}/>;
+  }
+  return (
+    <>
+      {ProductCardComponent}
+    </>
+  );
+}
+
+export default ProductDetails;
+
+ProductDetails.getLayout = function getLayout(page) {
+  return <NoFooterLayout>{page}</NoFooterLayout>;
+};
+
+export const getStaticPaths = async () => {
+  const response = await fetch("http://localhost:3000/api/products");
+  const components = await response.json();
+  const paths = components.data.map((product) => ({
+    params: { productDetails: product._id },
+  }));
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const response = await fetch(
+    `http://localhost:3000/api/products?id=${params.productDetails}`
+  );
+  const product = await response.json();
+  console.log(product);
+  return {
+    props: {
+      singleProduct: product.data,
+    },
+  };
+};
