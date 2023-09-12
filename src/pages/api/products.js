64b4b9f64e29bb07cdbf1ff9d81865743c023f94
afzal mahmud <afzzalmahmud@gmail.com>
2023-09-12 23:23:12 +0600

    [Err-Fix] some error fixes by using condition on Api

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -1,5 +1,5 @@
 const { default: config } = require("@/config");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const url = config.database_url;
 
 const client = new MongoClient(url, {
@@ -13,29 +13,36 @@ const client = new MongoClient(url, {
 async function pcbuilder(req, res) {
   try {
     await client.connect();
-    const componentsCollections = client
-      .db("nextjspc")
-      .collection("pc_components");
+    const componentsCollections = client.db("nextjspc").collection("pc_components");
 
     if (req.method === "GET") {
       if (req.query.category) {
-        /* finding componentsType based on query such as Monitor,Ram,Motherboard and other type */
+        // Handle GET request with a specific category
         const query_ProductType = req.query.category;
-        const cursor = await componentsCollections.find({
-          ProductType: query_ProductType,
-        });
-
+        const cursor = await componentsCollections.find({ ProductType: query_ProductType });
         const selectedComponents = await cursor.toArray();
         res.send({
           componentType: query_ProductType,
           statusCode: 200,
           data: selectedComponents,
         });
+      } else if (req.query.id) {
+        // Handle GET request with a specific ID
+        const query_ProductId = req.query.id;
+        const objectId = new ObjectId(query_ProductId);
+        const singleComponent = await componentsCollections.findOne({ _id: objectId });
+        res.send({
+          message:"success",
+          componentType: "single components",
+          statusCode: 200,
+          data: singleComponent,
+        })
+      } else {
+        // Handle GET request for all pc components
+        const cursor = await componentsCollections.find({});
+        const allComponents = await cursor.toArray();
+        res.send({ message: "success", statusCode: 200, data: allComponents });
       }
-      /* geting all pc components */
-      const cursor = await componentsCollections.find({});
-      const allComponents = await cursor.toArray();
-      res.send({ message: "success", statusCode: 200, data: allComponents });
     }
   } finally {
     // await client.close();
